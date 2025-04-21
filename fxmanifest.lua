668871bc19643ed3557c3d0f0f75535113adab5d
Abonker <89600586+Abonker@users.noreply.github.com>
2025-04-21 10:52:40 +0200

    fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Abonker'
+description 'ESX Trucker job'
+version '1.0.0'
+
+shared_script '@es_extended/imports.lua'
+
+shared_script 'config.lua'
+client_script 'client.lua'
+server_script 'server.lua'
+
+dependency 'es_extended'
